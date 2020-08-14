f1e8ea52d51b3db8ef6c9706588aa59dd659cda4
CSBlues <laforests@ashevilleschool.org>
2020-08-14 17:27:56 -0400

    initial commit

---
+++ Playground Basics.playground/Pages/Wrapup.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+/*:
+## Wrapup
+In this playground, you’ve had a quick tour of playgrounds and the results sidebar. You’ve also learned a number of things to get you on the road to programming:
+- Doing mathematical calculations
+- Using comments
+- Troubleshooting errors when things go wrong
+
+Congratulations!
+ 
+- Important:Reflection Questions\
+ Q: What surprised you most about using the playground in this lesson?\
+ A:
+ \
+ Q: What other kinds of calculations could you store and run in a playground?\
+ A:
+
+ Excerpt From: Apple Education. “Develop in Swift AP CS Principles.” Apple Inc. - Education, 2020. Apple Books. https://books.apple.com/us/book/develop-in-swift-ap-cs-principles/id1511184045
+
+
+ 
+  _Copyright © 2020 Apple Inc._
+ 
+ _Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:_
+ 
+ _The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software._
+ 
+ _THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE._
+ 
+[Previous](@previous)  |  page 7 of 7
+ */
